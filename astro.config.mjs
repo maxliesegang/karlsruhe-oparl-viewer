9fffbf2dbe89d90c9a464fc653cbacf9e0bbf33c
Maximilian Liesegang <max@liesegang.io>
2026-02-07 09:33:52 +0100

    Refactor astro.config.mjs to support dynamic base URL based on command environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 
+const PAGES_BASE = "/karlsruhe-oparl-viewer/";
+const runningInCi =
+  process.env.CI === "true" || process.env.GITHUB_ACTIONS === "true";
+const isDev = process.env.NODE_ENV === "development" && !runningInCi;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://maxliesegang.github.io",
-  base: "karlsruhe-oparl-viewer/",
+  base: isDev ? "/" : PAGES_BASE,
   build: {
     format: "file",
   },
